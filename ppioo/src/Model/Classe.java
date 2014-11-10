c8a96b73da64fd89cc31f5fb281df96bf228b65a
oassam <oassam@gmail.com>
2014-11-10 21:50:43 +0000

    Upando!

---
+++ ppioo/src/Model/Classe.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Model;
+
+/**
+ *
+ * @author Giovane
+ */
+public class Classe {
+    String nome;
+    int dano;
+    int defesa;
+    //TESTE DO SVN, LEMBRAR DE RETIRAR DEPOIS
+    
+}
