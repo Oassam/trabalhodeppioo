2c52b0ae95683e751d5ceda230a0e11d9aef198b
oassam@gmail.com <oassam@gmail.com@cca13aa1-537f-dee0-50e7-fbfdc3a5b020>
2014-11-12 09:58:07 +0000

    Adicionado classes Antagonista, Protagonista e Rotinas

--- trunk/ppioo/src/Model/Personagem.java
+++ trunk/ppioo/src/Model/Personagem.java
@@ -1,20 +1,51 @@
 package Model;
 
+import static Model.Rotinas.DEFENDEU;
+import static Model.Rotinas.SOFREU_DANO;
+
 public abstract class Personagem {
     
-  String nome;
+  private String nomePersonagem;
+  private int quantidadeVida;
   Classe classe;
-
-  public String getNome() {
-    return nome;
-  }
-
-  public void setNome(String nome) {
-    this.nome = nome;
-  }
 
   public abstract int getDano();
   
   public abstract int getResistencia(); 
+  
+  public abstract int atacar(Personagem personagemAtacado); 
+
+  public String getNome() {
+    return nomePersonagem;
+  }
+
+  public void setNome(String novoNome) {
+    this.nomePersonagem = novoNome;
+  }
+
+  public int getQuantidadeVida() {
+    return quantidadeVida;
+  }
+
+  public void setQuantidadeVida(int novaQuantidadeVida) {
+    this.quantidadeVida = novaQuantidadeVida;
+  }
+  
+  public void sofrerDano(int reducaoVida){
+    this.quantidadeVida = this.quantidadeVida - reducaoVida;
+  }
+  
+  public String reagir(int reducaoVida){
+    String saida;
+    boolean defesa = Rotinas.getRandomBoolean();
+    sofrerDano(reducaoVida);
+    saida = this.getNome() + SOFREU_DANO;
+    
+    if (defesa){
+      this.setQuantidadeVida(this.getResistencia());
+      saida = saida + DEFENDEU;
+    }
+    return saida;
+  }
   
 }
--- trunk/ppioo/src/View/Main.java
+++ trunk/ppioo/src/View/Main.java
@@ -1,8 +1,5 @@
 package View;
 
-import javax.swing.JPanel;
-import sun.java2d.d3d.D3DRenderQueue;
-
 public class Main extends javax.swing.JPanel {
 
     /**
@@ -13,71 +10,69 @@ public class Main extends javax.swing.JPanel {
     }
 
     @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
-    private void initComponents() {
+  // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+  private void initComponents() {
 
-        lblTitulo = new javax.swing.JLabel();
-        lblDescricao = new javax.swing.JLabel();
-        btnNovoJogo = new javax.swing.JButton();
-        btnSair = new javax.swing.JButton();
+    lblTitulo = new javax.swing.JLabel();
+    lblDescricao = new javax.swing.JLabel();
+    btnNovoJogo = new javax.swing.JButton();
+    btnSair = new javax.swing.JButton();
 
-        lblTitulo.setFont(new java.awt.Font("Pristina", 0, 36)); // NOI18N
-        lblTitulo.setForeground(new java.awt.Color(255, 0, 0));
-        lblTitulo.setText("UEM Warriors");
+    lblTitulo.setFont(new java.awt.Font("Pristina", 0, 36)); // NOI18N
+    lblTitulo.setForeground(new java.awt.Color(255, 0, 0));
+    lblTitulo.setText("UEM Warriors");
 
-        lblDescricao.setFont(new java.awt.Font("Pristina", 0, 24)); // NOI18N
-        lblDescricao.setForeground(new java.awt.Color(153, 0, 0));
-        lblDescricao.setText("Uma terra selvagem com desafios gigantescos");
+    lblDescricao.setFont(new java.awt.Font("Pristina", 0, 24)); // NOI18N
+    lblDescricao.setForeground(new java.awt.Color(153, 0, 0));
+    lblDescricao.setText("Uma terra selvagem com desafios gigantescos");
 
-        btnNovoJogo.setLabel("Novo Jogo");
-        btnNovoJogo.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnNovoJogoActionPerformed(evt);
-            }
-        });
+    btnNovoJogo.setLabel("Novo Jogo");
+    btnNovoJogo.addActionListener(new java.awt.event.ActionListener() {
+      public void actionPerformed(java.awt.event.ActionEvent evt) {
+        btnNovoJogoActionPerformed(evt);
+      }
+    });
 
-        btnSair.setText("Sair");
-        btnSair.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnSairActionPerformed(evt);
-            }
-        });
+    btnSair.setText("Sair");
+    btnSair.addActionListener(new java.awt.event.ActionListener() {
+      public void actionPerformed(java.awt.event.ActionEvent evt) {
+        btnSairActionPerformed(evt);
+      }
+    });
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
-        this.setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(36, 36, 36)
-                        .addComponent(lblDescricao))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(98, 98, 98)
-                        .addComponent(lblTitulo, javax.swing.GroupLayout.PREFERRED_SIZE, 218, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(192, 192, 192)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(btnNovoJogo, javax.swing.GroupLayout.DEFAULT_SIZE, 192, Short.MAX_VALUE)
-                            .addComponent(btnSair, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
-                .addContainerGap(53, Short.MAX_VALUE))
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(23, 23, 23)
-                .addComponent(lblTitulo)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(lblDescricao)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 97, Short.MAX_VALUE)
-                .addComponent(btnNovoJogo, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(btnSair, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
-        );
-
-        btnSair.getAccessibleContext().setAccessibleName("Sair");
-    }// </editor-fold>//GEN-END:initComponents
+    javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+    this.setLayout(layout);
+    layout.setHorizontalGroup(
+      layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+      .addGroup(layout.createSequentialGroup()
+        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+          .addGroup(layout.createSequentialGroup()
+            .addGap(36, 36, 36)
+            .addComponent(lblDescricao))
+          .addGroup(layout.createSequentialGroup()
+            .addGap(98, 98, 98)
+            .addComponent(lblTitulo, javax.swing.GroupLayout.PREFERRED_SIZE, 218, javax.swing.GroupLayout.PREFERRED_SIZE))
+          .addGroup(layout.createSequentialGroup()
+            .addGap(111, 111, 111)
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+              .addComponent(btnNovoJogo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+              .addComponent(btnSair, javax.swing.GroupLayout.PREFERRED_SIZE, 192, javax.swing.GroupLayout.PREFERRED_SIZE))))
+        .addContainerGap(61, Short.MAX_VALUE))
+    );
+    layout.setVerticalGroup(
+      layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+      .addGroup(layout.createSequentialGroup()
+        .addGap(23, 23, 23)
+        .addComponent(lblTitulo)
+        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+        .addComponent(lblDescricao)
+        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 97, Short.MAX_VALUE)
+        .addComponent(btnNovoJogo, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
+        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+        .addComponent(btnSair, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
+        .addContainerGap())
+    );
+  }// </editor-fold>//GEN-END:initComponents
 
     private void btnNovoJogoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnNovoJogoActionPerformed
        // TODO add your handling code here:
@@ -91,10 +86,10 @@ public class Main extends javax.swing.JPanel {
         System.exit(0);
     }
     
-    // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnNovoJogo;
-    private javax.swing.JButton btnSair;
-    private javax.swing.JLabel lblDescricao;
-    private javax.swing.JLabel lblTitulo;
-    // End of variables declaration//GEN-END:variables
+  // Variables declaration - do not modify//GEN-BEGIN:variables
+  private javax.swing.JButton btnNovoJogo;
+  private javax.swing.JButton btnSair;
+  private javax.swing.JLabel lblDescricao;
+  private javax.swing.JLabel lblTitulo;
+  // End of variables declaration//GEN-END:variables
 }
